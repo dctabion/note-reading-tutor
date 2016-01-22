3e9561096de5c7b2f8eba26976b41d310ecdbd0f
Daniel Tabion <dctabion@gmail.com>
2016-01-21 18:01:11 -0600

    move mappings.  fixed duplicate of note variable.  Midi detection, soundfont support, note drawing is now working

--- public/javascripts/playGame.js
+++ public/javascripts/playGame.js
@@ -1,7 +1,12 @@
+// delete this TODO
+var tone;
+
 window.onload = function() {
   console.log('game.js is running!!');
   testHelper();
 
+  // delete this TODO
+  var tone;
 
   // create a new deck of cards
   var flashcards = allCards;
@@ -9,8 +14,8 @@ window.onload = function() {
 
   // Setup flashcard display
   var flashcard = document.getElementById('flashcard');
-  var note = document.getElementById('note');
-  note.style.display = 'none';
+  var flashcardNote = document.getElementById('flashcardNote');
+  flashcardNote.style.display = 'none';
 
   // Setup soundfonts
   var ctx = new AudioContext();
@@ -37,10 +42,84 @@ window.onload = function() {
   // On click, generate a card and draw
   flashcard.addEventListener('click', function(){
     console.log('click');
-    note.style.display = 'block';
+    flashcardNote.style.display = 'block';
 
     var randCardIndex = getRandomInt(0, flashcards.length);
-    note.style.top = noteToPosition[flashcards[randCardIndex]];
+    flashcardNote.style.top = noteToPosition[flashcards[randCardIndex]];
   });
+
+  // SETUP MIDI ========================================
+  var midi, data, cmd, channel, type, note, velocity;
+
+    // request MIDI access
+  if (navigator.requestMIDIAccess) {
+      navigator.requestMIDIAccess({
+          sysex: false
+      }).then(onMIDISuccess, onMIDIFailure);
+  } else {
+      alert("No MIDI support in your browser.");
+  }
+
+  // midi functions
+  function onMIDISuccess(midiAccess) {
+      // when we get a succesful response, run this code
+      midi = midiAccess; // this is our raw MIDI data, inputs, outputs, and sysex status
+
+      var inputs = midi.inputs.values();
+      // loop over all available inputs and listen for any MIDI input
+      for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+          // each time there is a midi message call the onMIDIMessage function
+          input.value.onmidimessage = onMIDIMessage;
+      }
+  }
+
+  function onMIDIFailure(e) {
+      // when we get a failed response, run this code
+      log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
+  }
+
+  function onMIDIMessage(message) {
+    data = event.data,
+    cmd = data[0] >> 4,
+    channel = data[0] & 0xf,
+    type = data[0] & 0xf0, // channel agnostic message type. Thanks, Phil Burk.
+    note = data[1],
+    velocity = data[2];
+
+    switch(type){
+        case 144: // noteOn message
+            noteOn(note, velocity);
+            break;
+        case 128: // noteOff message
+            noteOff(note, velocity);
+            break;
+    }
+    logger('MIDI data', data);
+  }
+
+  function logger(label, data) {
+      data = data || null;
+      messages = label + " [channel: " + (data[0] & 0xf) + ", cmd: " + (data[0] >> 4) + ", type: " + (data[0] & 0xf0) + " , note: " + data[1] + " , velocity: " + data[2] + "]";
+      console.log(messages);
+  }
+
+
+  function noteOn(midiNote, velocity){
+    // Note pressed
+    if (velocity>0) {
+      // play note
+      tone = instrument.play('A0',ctx.currentTime, 5);
+    }
+    // Note released (velocity == 0)
+    else {
+      // kill note
+    }
+    console.log('noteOn()');
+  }
+
+  function noteOff(midiNote, velocity){
+    // do something when note is released
+    console.log('noteOff()');
+  }
 
 };
