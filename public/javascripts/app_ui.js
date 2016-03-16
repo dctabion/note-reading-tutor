b02658c46972ec197f9ba83a3e88af1819215c5b
Daniel Tabion <dctabion@gmail.com>
2016-03-16 16:49:24 -0500

    fixed instruction modal so video stops when modal closed

--- public/javascripts/app_ui.js
+++ public/javascripts/app_ui.js
@@ -1,6 +1,45 @@
 $(document).ready(function(){
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal-instructions-trigger').leanModal();
+  // $('.modal-instructions-trigger').leanModal(
+  //   {
+  //
+  //   });
+  //
+    var url = $(".video-container iframe").attr('src');
+    // var url;
+    console.log('url: ', url);
+
+    $('.modal-instructions-trigger').leanModal({
+          dismissible: true, // Modal can be dismissed by clicking outside of the modal
+          opacity: .5, // Opacity of modal background
+          in_duration: 300, // Transition in duration
+          out_duration: 200, // Transition out duration
+          ready: function() {
+            console.log('Ready');
+            $(".video-container, iframe").attr('src', url);
+
+          }, // Callback for Modal open
+          complete: function() {
+            console.log('Closed');
+            $(".video-container, iframe").attr('src', '');
+          } // Callback for Modal close
+        }
+      );
+
+  //   {
+  //   complete: function() {
+  //     console.log('closing modal');
+  //     player.stopVideo();
+  //   }   // Callback for Modal close
+  // });
+// );
+
+
+  $('.modal-trigger').leanModal({
+
+    }
+  );
+
 
   var page = $('#page').html();
 
